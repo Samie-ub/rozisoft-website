e84f560dd2384e69adbfbf011996fa48164469c9
Samie <xxxsamie39@gmail.com>
2023-10-12 06:30:16 +0500

    blog page updates

--- client/src/pages/BlogPage.jsx
+++ client/src/pages/BlogPage.jsx
@@ -31,13 +31,13 @@ function BlogPage() {
               <Grid item md={11} lg={10}>
                 <h1 className="heading-blog">{blogData.title}</h1>
               </Grid>
-              <Grid item xs={11} lg={10} py={5}>
+              <Grid item xs={11.5} lg={10} py={5}>
                 <div
                   className="blog-bg"
                   style={{ backgroundImage: `url(${blogData.coverImageUrl})` }}
                 ></div>
               </Grid>
-              <Grid item md={11} lg={10}>
+              <Grid item xs={11.5} md={11} lg={10}>
                 <h2 className="sub-heading">{blogData.subTitle}</h2>
                 <div
                   dangerouslySetInnerHTML={{ __html: blogData.content }}
@@ -53,3 +53,4 @@ function BlogPage() {
 }
 
 export default BlogPage;
+x
