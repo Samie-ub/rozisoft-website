1e22aa5601c659d8c9951edf16fd7fcbca57125e
Samie <xxxsamie39@gmail.com>
2023-10-23 04:46:51 +0500

    blog page setting

--- client/src/pages/BlogPage.jsx
+++ client/src/pages/BlogPage.jsx
@@ -7,10 +7,21 @@ import Header from "../component/Header";
 import Navbar from "../component/Navbar";
 import "../styles/blog-page-styles.css";
 import Loader from "../component/Loader";
+import { socialData } from "../content";
 function BlogPage() {
   const { blogId } = useParams();
   const [blogData, setBlogData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+
+  const formatDate = (inputDate) => {
+    const date = new Date(inputDate);
+  
+    const day = date.getDate();
+    const month = date.toLocaleString('default', { month: 'long' });
+    const year = date.getFullYear();
+  
+    return `${day} ${month} ${year}`;
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -27,42 +38,75 @@ function BlogPage() {
         console.error("Error fetching blog data:", error);
       });
   }, [blogId]);
+  const calculateReadingTime = (text) => {
+    if (text) {
+      const averageWPM = 200;
+      const wordCount = text.split(" ").length;
+      const readingTimeMinutes = Math.ceil(wordCount / averageWPM);
+      return readingTimeMinutes;
+    }
+    return 0;
+  };
 
+  const readingTime = blogData.content
+    ? calculateReadingTime(blogData.content)
+    : 0;
   return (
     <div className="blog-page" name="blog-post">
       {isLoading ? (
-        <Loader/>
-      ):(
+        <Loader />
+      ) : (
         <>
-         <Header />
-      <Navbar />
-      <div className="blog-styles">
-        <Grid container justifyContent={"center"}>
-          <Grid item xs={11} md={12} lg={11}>
-            <Grid container justifyContent={"center"} py={5}>
-              <Grid item md={11} lg={10}>
-                <h1 className="heading-blog">{blogData.title}</h1>
-              </Grid>
-              <Grid item xs={11.5} lg={10} py={5}>
-                <div
-                  className="blog-bg"
-                  style={{ backgroundImage: `url(${blogData.coverImageUrl})` }}
-                ></div>
-              </Grid>
-              <Grid item xs={11.5} md={11} lg={10}>
-                <h2 className="sub-heading">{blogData.subTitle}</h2>
-                <div
-                  dangerouslySetInnerHTML={{ __html: blogData.content }}
-                ></div>
+          <Header />
+          <Navbar />
+          <div className="blog-styles">
+            <Grid container justifyContent={"center"}>
+              <Grid item xs={11} md={12} lg={11}>
+                <Grid container justifyContent={"center"} py={5}>
+                 
+                  <Grid item xs={11.5} lg={12} >
+                    <div
+                      className="blog-bg"
+                      style={{
+                        backgroundImage: `url(${blogData.coverImageUrl})`,
+                      }}
+                    ></div>
+                    <div className="date-container">
+                      <span>{formatDate(blogData.date)}</span>
+                      <span>{`${readingTime} min read`}</span>
+                    </div>
+                  </Grid>
+                  <Grid item md={11} lg={10} py={5}>
+                    <h1 className="heading-blog">{blogData.title}</h1>
+                  </Grid>
+                  <Grid item xs={11.5} md={11} lg={10}>
+                    
+                    <div
+                    className="quill-container"
+                      dangerouslySetInnerHTML={{ __html: blogData.content }}
+                    ></div>
+
+                    <div className="blog-links">
+                      <div className="social">
+                        {socialData.map((content)=>{
+                          return(
+                            <a href={content.href} target="_blank" rel="no-ref">
+                              <img src={content.icon} alt="social media links" />
+                              
+                            </a>
+                          )
+                        })}
+                      </div>
+                        <p className="bookmark">rozisoft</p>
+                    </div>
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </Grid>
-      </div>
-      <Footer />
+          </div>
+          <Footer />
         </>
       )}
-     
     </div>
   );
 }
